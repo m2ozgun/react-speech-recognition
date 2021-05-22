a34d0ac4bcd2b05f6232d8baf8e8a29bc27060d3
Your Name Here <memozgun@gmail.com>
2021-05-22 22:44:47 +0300

    add boosting

--- src/RecognitionManager.js
+++ src/RecognitionManager.js
@@ -21,8 +21,81 @@ export default class RecognitionManager {
     this.setSpeechRecognition(SpeechRecognition)
 
     if (isAndroid()) {
-      this.updateFinalTranscript = debounce(this.updateFinalTranscript, 250, true)
+      this.updateFinalTranscript = debounce(
+        this.updateFinalTranscript,
+        250,
+        true,
+      )
     }
+
+    this.negotiationPhrases = [
+      'all the remaining',
+      "that's it",
+      'you take',
+      'I want',
+      'I want everything',
+      'I would like to',
+      'I would like',
+      'I want to',
+      'I need to',
+      'rest is yours',
+      'you can have the rest',
+      'I offer',
+      'I accept',
+      'you give me',
+      'all remaining',
+      'I agree',
+      'you can',
+      'I can give',
+      'I want',
+    ]
+    this.domainKeywords = [
+      'one apple',
+      'two apple',
+      'two apples',
+      'three apple',
+      'three apples',
+      'four apple',
+      'four apples',
+      'one banana',
+      'two banana',
+      'two bananas',
+      'three banana',
+      'three bananas',
+      'four banana',
+      'four bananas',
+      'one orange',
+      'two orange',
+      'two oranges',
+      'three orange',
+      'three oranges',
+      'four orange',
+      'four oranges',
+      'one watermelon',
+      'two watermelon',
+      'two watermelons',
+      'three watermelon',
+      'three watermelons',
+      'four watermelon',
+      'four watermelons',
+      'all apples',
+      'all oranges',
+      'all bananas',
+      'all watermelons',
+      'all of apples',
+      'all of oranges',
+      'all of bananas',
+      'all of watermelons',
+      'all of the apples',
+      'all of the oranges',
+      'all of the bananas',
+      'all of the watermelons',
+      'zero apple',
+      'zero orange',
+      'zero banana',
+      'zero watermelon',
+      'all of them',
+    ]
   }
 
   setSpeechRecognition(SpeechRecognition) {
@@ -30,10 +103,50 @@ export default class RecognitionManager {
       this.recognition = new SpeechRecognition()
       this.recognition.continuous = false
       this.recognition.interimResults = true
+      this.recognition.maxAlternatives = 3
       this.recognition.onresult = this.updateTranscript.bind(this)
       this.recognition.onend = this.onRecognitionDisconnect.bind(this)
       this.emitBrowserSupportsSpeechRecognitionChange(true)
     }
+  }
+
+  boost(eventResults) {
+    // Similarity bakılabilir.
+    // Anagram yapısı kullanılabilir.
+
+    // TODO: Levenshtein distance ile kontrol et.
+    // TODO: Message protocol for dynamic threshold (Levenshtein).
+    // TODO: Threshold 1'den basliyor.
+
+    let maxConfidence = 0
+    let maxConfidenceIndex = 0
+
+    console.log('Interim results')
+    for (let i = 0; i < eventResults.length; i++) {
+      let confidence = eventResults[i].confidence
+      for (let j = 0; j < this.domainKeywords.length; j++) {
+        if (eventResults[i].transcript.includes(this.domainKeywords[j])) {
+          // Check if any of the keywords are included in the domain keywords..
+          confidence += 40
+        }
+      }
+
+      for (let k = 0; k < this.negotiationPhrases.length; k++) {
+        if (eventResults[i].transcript.includes(this.negotiationPhrases[k])) {
+          // Check if any of the keywords are included in the domain keywords..
+          confidence += 30
+        }
+      }
+
+      if (confidence > maxConfidence) {
+        maxConfidence = confidence
+        maxConfidenceIndex = i
+      }
+      // console.log('Results: ' + eventResults[i].transcript);
+      // console.log('Results: ' + eventResults[i].confidence);
+    }
+
+    return eventResults[maxConfidenceIndex].transcript
   }
 
   subscribe(id, callbacks) {
@@ -66,11 +179,20 @@ export default class RecognitionManager {
     })
   }
 
-  emitBrowserSupportsSpeechRecognitionChange(browserSupportsSpeechRecognitionChange) {
+  emitBrowserSupportsSpeechRecognitionChange(
+    browserSupportsSpeechRecognitionChange,
+  ) {
     Object.keys(this.subscribers).forEach((id) => {
-      const { onBrowserSupportsSpeechRecognitionChange, onBrowserSupportsContinuousListeningChange } = this.subscribers[id]
-      onBrowserSupportsSpeechRecognitionChange(browserSupportsSpeechRecognitionChange)
-      onBrowserSupportsContinuousListeningChange(browserSupportsSpeechRecognitionChange)
+      const {
+        onBrowserSupportsSpeechRecognitionChange,
+        onBrowserSupportsContinuousListeningChange,
+      } = this.subscribers[id]
+      onBrowserSupportsSpeechRecognitionChange(
+        browserSupportsSpeechRecognitionChange,
+      )
+      onBrowserSupportsContinuousListeningChange(
+        browserSupportsSpeechRecognitionChange,
+      )
     })
   }
 
@@ -109,16 +231,23 @@ export default class RecognitionManager {
   }
 
   updateTranscript({ results, resultIndex }) {
-    const currentIndex = resultIndex === undefined ? results.length - 1 : resultIndex
+    const currentIndex =
+      resultIndex === undefined ? results.length - 1 : resultIndex
     this.interimTranscript = ''
     this.finalTranscript = ''
     for (let i = currentIndex; i < results.length; ++i) {
-      if (results[i].isFinal && (!isAndroid() || results[i][0].confidence > 0)) {
+      if (
+        results[i].isFinal &&
+        (!isAndroid() || results[i][0].confidence > 0)
+      ) {
+        const finalTranscript = results[i][0].transcript
+        finalTranscript += this.boost(results[i])
+
         this.updateFinalTranscript(results[i][0].transcript)
       } else {
         this.interimTranscript = concatTranscripts(
           this.interimTranscript,
-          results[i][0].transcript
+          results[i][0].transcript,
         )
       }
     }
@@ -139,7 +268,7 @@ export default class RecognitionManager {
   updateFinalTranscript(newFinalTranscript) {
     this.finalTranscript = concatTranscripts(
       this.finalTranscript,
-      newFinalTranscript
+      newFinalTranscript,
     )
   }
 
@@ -158,8 +287,12 @@ export default class RecognitionManager {
       if (this.listening) {
         await this.stopListening()
       }
-      this.recognition.continuous = isContinuousChanged ? continuous : this.recognition.continuous
-      this.recognition.lang = isLanguageChanged ? language : this.recognition.lang
+      this.recognition.continuous = isContinuousChanged
+        ? continuous
+        : this.recognition.continuous
+      this.recognition.lang = isLanguageChanged
+        ? language
+        : this.recognition.lang
     }
     if (!this.listening) {
       if (!this.recognition.continuous) {
@@ -178,7 +311,7 @@ export default class RecognitionManager {
   async abortListening() {
     this.disconnect('ABORT')
     this.emitListeningChange(false)
-    await new Promise(resolve => {
+    await new Promise((resolve) => {
       this.onStopListening = resolve
     })
   }
@@ -186,7 +319,7 @@ export default class RecognitionManager {
   async stopListening() {
     this.disconnect('STOP')
     this.emitListeningChange(false)
-    await new Promise(resolve => {
+    await new Promise((resolve) => {
       this.onStopListening = resolve
     })
   }
